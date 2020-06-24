86cde3522a5dd6625235e2053d5d1ec4c9c84df2
Rajan Maurya <rajanmaurya154@gmail.com>
2020-06-24 22:04:45 +0530

    release: Playstore release 1.1

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -6,11 +6,11 @@ object DefaultConfig {
 }
 
 object App {
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
 
 object Core {
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
 }
